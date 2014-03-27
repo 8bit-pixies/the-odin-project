d6ea1773b64cdd75480e7f48799706872489a576
Chapman Siu <chapm0n.siu@gmail.com>
2014-03-27 00:34:42 -0400

    I0 jumpstartlab ROR

---
+++ web-development-101/project-rails/blogger/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '2336ce46139b0f4eaee2076feedb0a6124136a7d6369d4ee4c506eb94309c941ae55ad10d6b94023f43be73965b5f061fdf95eefda9d0a852f50eb7fe4bec337'
