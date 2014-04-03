3c85c5eaf5e17e6f0d1d81056d1ce5387ee83d73
Chapman Siu <chapm0n.siu@gmail.com>
2014-04-03 05:20:23 -0400

    project complete

--- ruby-programming/event_manager/lib/event_manager.rb
+++ ruby-programming/event_manager/lib/event_manager.rb
@@ -1,5 +1,6 @@
 require "csv"
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "27d1271a79644030b32e0583c89fa9d7"
 
@@ -19,12 +20,26 @@ end
 
 puts "EventManager Initialized!"
 
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
-    name = row[:first_name]
-    zipcode = clean_zipcode(row[:zipcode])
-    
-    legislators_string = legislators_by_zipcode(zipcode)
-    
-    puts "#{name} #{zipcode} [#{legislators_string}]"
+  id = row[0]
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  Dir.mkdir("output") unless Dir.exists? "output"
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+
 end
